6113bf98d86d9ac3b8c39512ccfd81c828c3fdfa
coloro <colorolabs@gmail.com>
2018-08-09 16:14:01 -0400

    reducer

--- src/reducers/createReducer.js
+++ src/reducers/createReducer.js
@@ -1,15 +1,7 @@
-import * as types from '../actions/Types'
-import {stateToLocal} from '../actions'
-
-const saveState = {
-  [types.REMOVE_ALERT]: (state) => stateToLocal(state),
-  [types.PUSH_ALERT]: (state) => stateToLocal(state)
-}
-
-export default (initialState, handlers) => (state = initialState, action) => {
+export default (initialState, handlers, postState) => (state = initialState, action) => {
   let newState = handlers.hasOwnProperty(action.type) ? handlers[action.type](state, action) : state
-  if (saveState.hasOwnProperty(action.type)) {
-    saveState[action.type](newState)
+  if (postState.hasOwnProperty(action.type)) {
+    postState[action.type](newState)
   }
   return newState
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import createReducer from './createReducer'
 import * as types from '../actions/Types'
+import {stateToLocal} from '../actions'
 
 const alerts = createReducer([], {
   [types.SET_ALERTS]: (state, action) => action.alerts,
@@ -10,6 +11,9 @@ const alerts = createReducer([], {
     alerts.splice(action.index, 1)
     return alerts
   }
+}, {
+  [types.REMOVE_ALERT]: stateToLocal,
+  [types.PUSH_ALERT]: stateToLocal
 })
 
 export default combineReducers({alerts})
